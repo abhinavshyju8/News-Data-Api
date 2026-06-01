ea25d1ddbf75d2e64f10e0195387aab9436dcf06
abhinavshyju8 <abhinavshyju8@gmail.com>
2026-06-01 14:10:21 +0530

    Database credentials are loaded from environment variables and used to establish a connection.

--- News-Api.py
+++ News-Api.py
@@ -5,3 +5,11 @@ from textblob import TextBlob
 from datetime import datetime
 
 API_KEY = os.getenv("NEWS_API_KEY")
+
+conn = psycopg2.connect(
+    host=os.getenv("DB_HOST"),
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    port=os.getenv("DB_PORT")
+)
