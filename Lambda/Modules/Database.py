7b4cc75bf0f2547675e64b47736e195ab0e999a7
abhinavshyju8 <abhinavshyju8@gmail.com>
2026-06-02 14:38:36 +0530

    added database module

---
+++ Lambda/Modules/Database.py
@@ -0,0 +1,41 @@
+
+from Lambda.Modules.config import DB_HOST, DB_PASSWORD, DB_PORT, DB_USER , DB_NAME, DB_HOST
+import pg8000
+
+from modules.config import *
+
+def get_connection():
+
+    conn = pg8000.connect(
+        host=DB_HOST,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        port=DB_PORT,
+        ssl_context=True
+    )
+
+    return conn
+
+
+def insert_news(cur, title, source, published_at,
+                sentiment_score, sentiment):
+
+    cur.execute(
+        """
+        INSERT INTO news_sentiment
+        (title, source, published_at,
+         sentiment_score, sentiment)
+
+        VALUES (%s, %s, %s, %s, %s)
+        """,
+        (
+            title,
+            source,
+            published_at,
+            sentiment_score,
+            sentiment
+        )
+    )
+database.py
+Displaying config.py.
